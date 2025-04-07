59bdc4cb211c0487a3db9a58d91335f0286cac66
nikka-cloud <bis00019102@gmail.com>
2025-04-07 17:57:17 +0500

    Add recipe data handling service
    Built a service to manage our recipe collection. It lets us
    get all recipes, find one by ID, add new recipes, change
    existing ones, and remove recipes we don't need anymore.
    Right now, it's just keeping the recipes in memory

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../../controllers/recipes');
+let recipes = [
+    {
+      id: '1',
+      title: 'Spaghetti Carbonara',
+      image: '/images/carbonara.jpg',
+      ingredients: ['Spaghetti', 'Eggs', 'Cheese', 'Pancetta'],
+      steps: 'Boil, fry pancetta, mix with eggs and cheese, combine.'
+    }
+  ];
+  
+  exports.getAll = () => recipes;
+  
+  exports.getById = id => recipes.find(r => r.id === id);
+  
+  exports.create = data => {
+    const newRecipe = {
+      id: Date.now().toString(),
+      ...data
+    };
+    recipes.push(newRecipe);
+  };
+  
+  exports.update = (id, data) => {
+    const index = recipes.findIndex(r => r.id === id);
+    if (index !== -1) {
+      recipes[index] = { ...recipes[index], ...data };
+    }
+  };
+  
+  exports.delete = id => {
+    recipes = recipes.filter(r => r.id !== id);
+  };
 
-router.get('/', controller.listRecipes);
-router.get('/new', controller.newRecipeForm);
-router.post('/', controller.createRecipe);
-router.get('/:id', controller.recipeDetail);
-router.get('/:id/edit', controller.editRecipeForm);
-router.post('/:id', controller.updateRecipe);
-router.post('/:id/delete', controller.deleteRecipe);
-
-module.exports = router;
+  
