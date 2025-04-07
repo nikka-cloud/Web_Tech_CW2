95bbf44e241643d9490697b8b32dda06a37461c1
nikka-cloud <bis00019102@gmail.com>
2025-04-07 17:52:23 +0500

    Add recipe routes
    Provides a concise overview of the routes defined like list, create,
    view details, edit, update, and delete and their mapping to controller functions.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../../controllers/recipes');
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Culinary Hub' });
-});
+router.get('/', controller.listRecipes);
+router.get('/new', controller.newRecipeForm);
+router.post('/', controller.createRecipe);
+router.get('/:id', controller.recipeDetail);
+router.get('/:id/edit', controller.editRecipeForm);
+router.post('/:id', controller.updateRecipe);
+router.post('/:id/delete', controller.deleteRecipe);
 
 module.exports = router;
